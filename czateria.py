ec9397fd55ca010c6830d018b45a14ab8887c3d7
konradk84 <konrad.karasinski84@gmail.com>
2019-08-27 23:17:04 +0200

    google query

--- czateria.py
+++ czateria.py
@@ -28,12 +28,14 @@ def readConfig(chat):
     #configParams = configParams.append(chat, login, password)
     return configParams
 
-def setupBrowser():
-    opts = Options()
-    opts.set_headless()
-    assert opts.headless
-    browser = webdriver.Firefox(options=opts)
-    #browser = webdriver.Firefox()
+def setupBrowser(browserWindow):
+    if browserWindow == True:
+        browser = webdriver.Firefox()
+    elif browserWindow == False:
+        opts = Options()
+        opts.set_headless()
+        assert opts.headless
+        browser = webdriver.Firefox(options=opts)
     return browser
 
 def joinChat(browser, chat, login, password):
@@ -53,9 +55,11 @@ def joinChat(browser, chat, login, password):
 
 def getFiveLastElements(browser):
     elements = browser.find_elements_by_xpath("//*[@class='m-msg-item']")
+    accosted = browser.find_elements_by_xpath("//*[@class='m-msg-item accosted']")
+    elements = elements + accosted
     #print("elements type: ", type(elements)) #list
     listElements = []
-    for intElement in range(len(elements)-10, len(elements)):
+    for intElement in range(len(elements)-12, len(elements)):
         #print(elements[intElement].text) #string
         listElements.append(elements[intElement].text)
     return listElements
@@ -75,9 +79,26 @@ def compareListsElement(listElements, oldListElements):
     #print("\ndiff: ", returnedSet)
     return returnedSet 
 
+def randomRespond(query):
+    try:
+        browserWindow = True
+        browser = setupBrowser(browserWindow)
+        browser.get("https://google.pl")
+        search = browser.find_element_by_name('q')
+        search.send_keys(query)
+        search.send_keys(Keys.RETURN)
+        time.sleep(2)
+        search = browser.find_element_by_class_name("r").click()
+        time.sleep(2)
+        content = browser.find_element_by_tag_name("body")
+        #print(content.text)
+    finally:
+        browser.quit()
+
 try:
     configParams = readConfig(selectChat())
-    browser = setupBrowser()
+    browserWindow = False
+    browser = setupBrowser(browserWindow)
     joinChat(browser, configParams[0], configParams[1], configParams[2])
     oldListElements = getFiveLastElements(browser)
     time.sleep(5)
@@ -87,28 +108,39 @@ try:
         uniqueList = list(uniqueSet)
         #print("unique: ", uniqueList)
         for unique in uniqueList:
+            time.sleep(1)
             print("MSG: ", unique)
             listNickMsg = unique.split(": ", 1)
-            #print("MSG: ", listNickMsg)
+            print("split: ", listNickMsg)
             if len(listNickMsg) == 2:
-                if listNickMsg[0] == 'kot32' and listNickMsg[1] == "kim jestem":
+                if listNickMsg[0] == 'kot32' and listNickMsg[1] == "kim jestem?":
                     #print("tak")
                     #username = browser.find_element_by_id("nick-login").send_keys("zwyciezca!", Keys.Enter)
-                    browser.find_element_by_class_name("text-input").send_keys("zwyciezca!", Keys.ENTER)
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": jesteś zwyciężcą!", Keys.ENTER)
                 if listNickMsg[0] == 'kot32' and listNickMsg[1] == "w!":
-                    browser.find_element_by_class_name("text-input").send_keys("papatki", Keys.ENTER)
+                    #browser.find_element_by_class_name("text-input").send_keys(":(", Keys.ENTER)
                     print("#### jest wyjdz ####")
                     browser.quit()
                 if (str(listNickMsg[1]).find("hej")) != -1 or (str(listNickMsg[1]).find("hejka")) != -1 or (str(listNickMsg[1]).find("witam")) != -1 or (str(listNickMsg[1]).find("czesc")) != -1 or (str(listNickMsg[1]).find("cześć")) != -1:
-                    browser.find_element_by_class_name("text-input").send_keys("kłaniam się nisko", Keys.ENTER)
-                #if (str(listNickMsg[1]).find("Ashele xD")) != -1 or (str(listNickMsg[1]).find("kot32 xD")):
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+ ": witaj :)", Keys.ENTER)
+                if (str(listNickMsg[1]).find("klakier_xD ")) != -1:
+                    #browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+ ": :-/", Keys.ENTER)
+                    query = str(listNickMsg[1])[len("klakier_xD"):]
+                    respond = randomRespond(query)
+
                 if unique == "kot32: xD" or unique == "Ashele: xD":
                     browser.find_element_by_class_name("text-input").send_keys("xD", Keys.ENTER)
-                if (str(listNickMsg[1]).find("ide")) != -1:
-                    browser.find_element_by_class_name("text-input").send_keys("idź", Keys.ENTER)
-                   #print("tak")
-                    #username = browser.find_element_by_id("nick-login").send_keys("zwyciezca!", Keys.Enter)
-                    #browser.find_element_by_class_name("text-input").send_keys("zwyciezca!", Keys.ENTER)
+                if (str(listNickMsg[1]).find("ide")) != -1 or (str(listNickMsg[1]).find("idę")) != -1:
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": idź", Keys.ENTER)
+                if (str(listNickMsg[1]).find("skarbus")) != -1 or (str(listNickMsg[1]).find("skarbuś")) != -1:
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": skarbuś... <u-rzyganie>", Keys.ENTER)
+                if (str(listNickMsg[1]).find("ssij")) != -1:
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": zrób mi <u-lodzik2>", Keys.ENTER)
+                if (str(listNickMsg[1]).find("brak bolca")) != -1:
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": brak stolca?:o", Keys.ENTER)     
+                if (str(listNickMsg[1]).find("brak stolca")) != -1:
+                    browser.find_element_by_class_name("text-input").send_keys((listNickMsg[0])+": brak bolca?:o", Keys.ENTER) 
+                
             if len(listNickMsg) == 1:
                 if listNickMsg[0] == "+ kot32 wszedł":
                     browser.find_element_by_class_name("text-input").send_keys("siema kocie", Keys.ENTER)
@@ -123,41 +155,10 @@ try:
                 if listNickMsg[0] == "+ CuKierKoWa:-)_ wszedł":
                     browser.find_element_by_class_name("text-input").send_keys("CuKierKoWa:-)_ no cześć maleńka:*", Keys.ENTER) 
                 
-                
-                #TODO: <u-yspie>
                 #if listNickMsg[0] == "+ yenn wszedł":
                 #    browser.find_element_by_class_name("text-input").send_keys("o jezusiku :o", Keys.ENTER)       
-                    #print("jest lamus")
-            #if bytearray(listNickMsg[0], 'utf-8').decode() == "kot32 wsze":
-            #    print("jest wszedl")
-            
-            #print(type(listNickMsg))
-            #print("0: ", listNickMsg[0])
-            #print(len(listNickMsg))
-        
-        
+        #robimy kopie elementow do sprawdzenia w nastpnej iteracji by nie dublowac
         oldListElements = listElements
-        #respond(uniqueList)
-        
-
-        #print("mamy: ", listElements)
-        #print(type(listElements))
-    
         time.sleep(10)
-    
-    #while True:
-        #print(elements[2].text)
-        #print("prevElements type: ", type(prevElements))
-        
-        #fun getFiveLastElements
-        #look for any of last five in last 10
-        #do some stuff
-        #set last 10 elements
-        #repeat
-                #print(intElement)
-     #           compareElements(elements[intElement].text, prevElements)
-      #          prevElements = prevElements.append(elements[intElement].text)
-        #prevElements = elements
 finally:
-    #input("press any key to exit")
     browser.quit()
